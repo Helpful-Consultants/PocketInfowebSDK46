cbb988c8876bff7d42afad9cb615095047cacf3a
Alan Upstone <alan@helpfulconsultants.com>
2019-12-12 15:21:06 +0000

    tidy up logs

--- constants/Urls.js
+++ constants/Urls.js
@@ -4,6 +4,7 @@ export default {
   toolsInfoweb: serverAddress,
   newsHeadlineImage: serverAddress + '/content/images/headlines/',
   productsHeadlineImage: serverAddress + '/content/images/headlines/',
+  ltpImage: serverAddress + '/loanToolProgramme/content/images/toolProfile/',
   toolImage: serverAddress + '/mandatoryList/content/images/toolsList/'
   //   toolImage: serverAddress + '/mandatoryList/content/images/icons/'
 };
