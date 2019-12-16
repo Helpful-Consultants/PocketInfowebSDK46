08fb1cd8cef60f60d0a84473980c5d1492f6bbc7
Alan Upstone <alan@helpfulconsultants.com>
2019-12-16 11:34:21 +0000

    page title colour

--- components/TitleWithAppLogo.js
+++ components/TitleWithAppLogo.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Image, Text } from 'react-native-elements';
-import appLogo from '../assets/images/logos/tiw-app-logo-trans.png';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import Colors from '../constants/Colors';
 
 export default function getLogoTitle(props) {
   return (
     <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-      <Image source={appLogo} style={{ width: 26, height: 26 }} />
+      <Image
+        source={require('../assets/images/logos/tiw-app-logo-trans.png')}
+        style={{ width: 26, height: 26 }}
+      />
       <Text
         style={{
           fontWeight: 'bold',
-          fontSize: 12,
-          paddingLeft: 5
+          fontSize: RFPercentage(2.2),
+          paddingLeft: 5,
+          color: Colors.vwgBlack
         }}
       >
         {props.title}
