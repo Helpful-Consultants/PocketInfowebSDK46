780052dff8c48065d345ec9aba953ecae760c117
Alan Upstone <alan@helpfulconsultants.com>
2019-12-17 14:32:09 +0000

    new news dummy data

--- dummyData/newsDummyData.js
+++ dummyData/newsDummyData.js
@@ -1,5 +1,59 @@
 export default newsDummyData = [
   {
+    createdDate: '31/08/2018 11:20:33',
+    headline: 'Tools & Equipment: Blocked & Back Orders December 2019',
+    id: '4',
+    imageName: 'deliveryTruckTimings.png',
+    ipAddress: '86.188.233.41',
+    lastUpdated: '16/12/2019 11:19:24',
+    linkTo: '?controller=stories&action=view&id=38',
+    newsType: 'News',
+    newstext:
+      'The Tools and Equipment department in AG are seeing an ever increasing demand for key inventory items, mainly diagnostic equipment. This increase in demand is having significant impact on all markets and their ability to fulfil current demand. ',
+    updatedBy: 'Lyndon Evans'
+  },
+  {
+    createdDate: '10/12/2019 09:35:55',
+    headline: 'Diagnostic Equipment Windows 10 Upgrade',
+    id: '31',
+    imageName: 'windows10Logo.png',
+    ipAddress: '165.225.80.226',
+    lastUpdated: '10/12/2019 09:37:08',
+    linkTo:
+      'https://toolsinfoweb.co.uk/Content/Documents/desktopBulletins/Windows 10/Windows_10_Upgrade_comms.pdf',
+    newsType: 'News',
+    newstext:
+      'After 2019, Windows 7 will no longer be supported by Microsoft and by our Offboard Diagnostic Information System Service ODiS.Our latest generation of diagnostic units will be unaffected by this, as they are supplied with Enterprise 2016 LTSB, a special version of Microsoft Windows 10.',
+    updatedBy: 'Simon Groves'
+  },
+  {
+    createdDate: '11/11/2019 16:18:27',
+    headline: 'Volkswagen Group service equipment from Beissbarth',
+    id: '26',
+    imageName: 'everardBeissbarth.png',
+    ipAddress: '86.188.233.41',
+    lastUpdated: '18/11/2019 16:45:41',
+    linkTo:
+      'https://toolsinfoweb.co.uk/Content/Documents/EverardBeissbarth-VW-001.pdf',
+    newsType: 'News',
+    newstext:
+      'Beissbarth, manufacturers of precision measuring equipment for OEM car manufacturers is exclusively distributed in the UK by Everard Group Ltd. including all UK sales, parts and servicing activities.',
+    updatedBy: 'Lyndon Evans'
+  },
+  {
+    createdDate: '18/11/2019 15:53:35',
+    headline: 'Software update for VAS 6910',
+    id: '30',
+    imageName: 'VAS_6910.png',
+    ipAddress: '165.225.80.61',
+    lastUpdated: '18/11/2019 16:01:20',
+    linkTo: 'Content/Documents/VAS_6910_Battery_Balancer_SoftwareUpdates.pdf',
+    newsType: 'News',
+    newstext:
+      'Voltage balancing device for high-voltage battery module (module balancer).  If you need to update software for your VAS 6910, here are instructions on how to do that.',
+    updatedBy: 'Simon Groves'
+  },
+  {
     createdDate: '25/10/2019 07:39:52',
     headline: 'VAS 6161 Verification Testing',
     id: '25',
@@ -11,32 +65,6 @@ export default newsDummyData = [
     newsType: 'News',
     newstext:
       'To ensure your VAS 6161 is performing to factory specification it is recommended by Rotronics and Volkswagen Group that a 12 monthly verification is performed. This will help support operational demands on the tester.',
-    updatedBy: 'Simon Groves'
-  },
-  {
-    createdDate: '31/08/2018 11:20:33',
-    headline: 'Tools & Equipment: Blocked & Back Orders Oct 2019',
-    id: '4',
-    imageName: 'deliveryTruckTimings.png',
-    ipAddress: '86.188.233.41',
-    lastUpdated: '20/09/2019 08:33:08',
-    linkTo: 'https://toolsinfoweb.co.uk/?controller=stories&action=view&id=38',
-    newsType: 'News',
-    newstext:
-      'The Tools and Equipment department in AG are seeing an ever increasing demand for key inventory items, mainly diagnostic equipment. This increase in demand is having significant impact on all markets and their ability to fulfil current demand. ',
-    updatedBy: 'Lyndon Evans'
-  },
-  {
-    createdDate: '30/11/2018 12:04:18',
-    headline: 'Windows 10',
-    id: '18',
-    imageName: 'windows10Logo.png',
-    ipAddress: '86.159.21.161',
-    lastUpdated: '06/09/2019 10:35:39',
-    linkTo:
-      'https://www.toolsinfoweb.co.uk/Content/Documents/desktopBulletins/Windows%2010/Windows_10_Upgrade_Comms.pdf',
-    newsType: 'News',
-    newstext: 'Windows 10 Upgrade - Compatible VAS Diagnostic Equipment',
     updatedBy: 'Simon Groves'
   },
   {
@@ -53,19 +81,6 @@ export default newsDummyData = [
     updatedBy: 'Lyndon Evans'
   },
   {
-    createdDate: '28/02/2019 11:00:18',
-    headline: 'Please Check the contacts we have for your site',
-    id: '21',
-    imageName: 'contactsRolodex.png',
-    ipAddress: '86.132.21.59',
-    lastUpdated: '28/02/2019 11:02:49',
-    linkTo: '?controller=dealers&action=dealerContactsCheck',
-    newsType: 'News',
-    newstext:
-      'We will soon be forcing you to check your contact details on a regular basis. Why not get ahead of the game? Please review the list of contacts that we have for your location and let us know any colleagues who have left.',
-    updatedBy: 'Simon Groves'
-  },
-  {
     createdDate: '13/12/2018 20:51:33',
     headline: 'VW AG Sales Catalogue',
     id: '20',
@@ -80,27 +95,13 @@ export default newsDummyData = [
     updatedBy: 'Simon Groves'
   },
   {
-    createdDate: '31/08/2018 11:16:33',
-    headline: 'New ordering and delivery process for special tools',
-    id: '1',
-    imageName: 'matraBoxesWarehouse.png',
-    ipAddress: '86.177.21.164',
-    lastUpdated: '08/12/2018 07:28:08',
-    linkTo: 'Content/Documents/Library/Matra_Supply_And_Order_Type.pdf',
-    newsType: 'News',
-    newstext:
-      'As of 1st January 2018, the way in which special tools are ordered was changed. This is a result of MATRA switching to being a production partner for Volkswagen Group.',
-    updatedBy: 'Simon Groves'
-  },
-  {
     createdDate: '08/11/2018 13:37:36',
     headline: 'VAS 6154 Aftersales Support',
     id: '10',
     imageName: 'VAS6154.png',
     ipAddress: '165.225.80.226',
     lastUpdated: '04/12/2018 14:43:41',
-    linkTo:
-      'https://toolsinfoweb.co.uk/?controller=desktopBulletins&action=list',
+    linkTo: '?controller=desktopBulletins&action=list',
     newsType: 'News',
     newstext:
       'There is a clear Aftersales Support process for issues involving VAS 6154. For further details, refer to the process description.',
