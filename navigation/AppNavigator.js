42d6ed1520f4c784abdadaa7c3a367c8a3437718
Alan Upstone <alan@helpfulconsultants.com>
2019-12-20 12:42:09 +0000

    custom drawer navigator

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
+// import {
+//   createDrawerNavigator,
+//   DrawerNavigatorItems as DrawerItems
+// } from 'react-navigation-drawer';
+import Constants from 'expo-constants';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import SignInScreen from '../screens/SignInScreen';
 import SignOutScreen from '../screens/SignOutScreen';
@@ -80,52 +86,73 @@ const SignedOutStack = createStackNavigator({
 //   }
 // });
 
-const AppDrawerNavigator = createDrawerNavigator({
-  HomeScreen: {
-    screen: HomeScreen,
-    navigationOptions: { drawerLabel: 'Home' }
-  },
-  FindToolsStack: {
-    screen: WipTabNavigator,
-    navigationOptions: {
-      drawerLabel: 'Tools & jobs',
-      initialRouteName: 'FindTools'
+const DrawerContent = props => (
+  <ScrollView style={styles.container}>
+    <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+      <View style={styles.stretched}>
+        <DrawerItems {...props} />
+        <View style={styles.footerContainer}>
+          <Text style={styles.appName}>{Constants.manifest.name}</Text>
+          <Text
+            style={styles.appVersion}
+          >{`Version ${Constants.manifest.version}`}</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  </ScrollView>
+);
+
+const AppDrawerNavigator = createDrawerNavigator(
+  {
+    HomeScreen: {
+      screen: HomeScreen,
+      navigationOptions: { drawerLabel: 'Home' }
+    },
+    FindToolsStack: {
+      screen: WipTabNavigator,
+      navigationOptions: {
+        drawerLabel: 'Tools & jobs',
+        initialRouteName: 'FindTools'
+      }
+    },
+    NewsStack: {
+      screen: NewsTabNavigator,
+      navigationOptions: {
+        drawerLabel: 'News & products',
+        initialRouteName: 'News'
+      }
+    },
+    OdisStack: {
+      screen: NewsTabNavigator,
+      navigationOptions: {
+        drawerLabel: 'Odis versions',
+        initialRouteName: 'Odis'
+      }
+    },
+    StatsStack: {
+      screen: NewsTabNavigator,
+      navigationOptions: { drawerLabel: 'Stats', initialRouteName: 'Stats' }
+    },
+    SignedOutStack: {
+      screen: SignedOutStack,
+      navigationOptions: {
+        drawerLabel: 'Sign out'
+      }
     }
   },
-  NewsStack: {
-    screen: NewsTabNavigator,
-    navigationOptions: {
-      drawerLabel: 'News & products',
-      initialRouteName: 'News'
-    }
-  },
-  OdisStack: {
-    screen: NewsTabNavigator,
-    navigationOptions: {
-      drawerLabel: 'Odis versions',
-      initialRouteName: 'Odis'
-    }
-  },
-  StatsStack: {
-    screen: NewsTabNavigator,
-    navigationOptions: { drawerLabel: 'Stats', initialRouteName: 'Stats' }
-  },
-  SignedOutStack: {
-    screen: SignedOutStack,
-    navigationOptions: {
-      drawerLabel: 'Sign out'
-    }
+  {
+    contentComponent: DrawerContent
   }
-});
+);
 
 // Tab navigator
-const MainTabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Wip: WipTabNavigator,
-  News: NewsTabNavigator
-});
+// const MainTabNavigator = createBottomTabNavigator({
+//   Home: HomeScreen,
+//   Wip: WipTabNavigator,
+//   News: NewsTabNavigator
+// });
 
-MainTabNavigator.path = '';
+// MainTabNavigator.path = '';
 
 export default createAppContainer(
   createSwitchNavigator(
@@ -133,8 +160,37 @@ export default createAppContainer(
       AuthLoading: AuthLoadingScreen,
       Auth: SignedOutStack,
       Main: AppDrawerNavigator
+      //   Main: CustomDrawerContentComponent
     },
     // { initialRouteName: userIsSignedIn ? 'Main' : 'Auth' }
     { initialRouteName: 'AuthLoading' }
   )
 );
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  zzstretched: {
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'space-between'
+    // backgroundColor: 'red'
+  },
+  footerContainer: {
+    // flex: 1,
+    // flexDirection: 'column',
+    // alignItems: 'flex-start',
+    // justifyContent: 'flex-end',
+    // backgroundColor: 'red'
+  },
+  appName: {
+    paddingTop: 100,
+    paddingLeft: 18
+    // fontStyle: 'italic'
+  },
+  appVersion: {
+    paddingTop: 5,
+    paddingLeft: 18
+    // fontStyle: 'italic'
+  }
+});
