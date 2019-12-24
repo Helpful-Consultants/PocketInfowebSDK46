01e420d4e4518be75b278740cbaed61db4802616
Alan Upstone <alan@helpfulconsultants.com>
2019-12-24 12:50:49 +0000

    add empty wips to types

--- constants/Types.js
+++ constants/Types.js
@@ -56,6 +56,7 @@ export default {
   CREATE_DEALER_WIP_REQUEST: 'dealerWips/create_dealer_wip_request',
   CREATE_DEALER_WIP_SUCCESS: 'dealerWips/create_dealer_wip_success',
   UPDATE_DEALER_WIP_REQUEST: 'dealerWips/update_dealer_wip_request',
+  EMPTY_DEALER_WIPS_REQUEST: 'dealerWips/empty_dealer_wips_request',
   DEALER_WIPS_ERROR: 'dealerWips/dealer_wips_error',
 
   FORM_INPUT_UPDATE: 'FORM_INPUT_UPDATE'
