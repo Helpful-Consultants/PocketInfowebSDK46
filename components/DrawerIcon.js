3284a6dea4b39cb4d580fa1d39f75965c0748ce7
Alan Upstone <alan@helpfulconsultants.com>
2020-01-21 17:27:42 +0000

    new drawer icon

--- components/DrawerIcon.js
+++ components/DrawerIcon.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
+import { RFPercentage } from 'react-native-responsive-fontsize';
 import Colors from '../constants/Colors';
+var iconSize = RFPercentage(2.5);
 
 export default function DrawerIcon(props) {
   return (
     <Ionicons
       name={props.name}
-      size={18}
-      style={{ marginBottom: -3, marginLeft: 0, paddingLeft: 0 }}
-      color={props.tintColor}
+      size={iconSize}
+      style={{ marginBottom: 0, marginLeft: 0, paddingLeft: 0 }}
+      color={Colors.vwgIosLink}
     />
   );
 }
