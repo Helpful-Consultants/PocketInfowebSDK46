7876cb4ca43de385d16fc44b4fe164edbb6fa909
Alan Upstone <alan@helpfulconsultants.com>
2020-01-21 17:31:54 +0000

    new components currently not used

---
+++ components/MenuDrawer.js
@@ -0,0 +1,53 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { Platform, ScrollView, StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-elements';
+
+import Modal from 'react-native-modal';
+
+import Colors from '../constants/Colors';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+
+export default function MenuDrawer(props) {
+  console.log('in MenuDrawer');
+  console.log(props);
+  let drawer = (
+    <Modal
+      isVisible={props.isVisible}
+      onBackdropPress={() => props.backdropPressHandler()}
+      onSwipeComplete={() => props.setIsBasketVisible(false)}
+      propagateSwipe
+      style={styles.menuDrawer}
+      backdropOpacity={0.6}
+      animationIn='zoomInDown'
+      animationOut='zoomOutUp'
+    >
+      <View style={{ backgroundColor: 'red' }}>
+        <Text>Stuff</Text>
+        <Text>Stuff</Text>
+        <Text>Stuff</Text>
+        <Text>Stuff</Text>
+        <Text>Stuff</Text>
+        <Text>Stuff</Text>
+        <Text>Stuff</Text>
+        <Text>Stuff</Text>
+        <Text>Stuff</Text>
+      </View>
+    </Modal>
+  );
+  console.log(drawer);
+  return drawer;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    backgroundColor: '#fff'
+  },
+  menuDrawer: {
+    justifyContent: 'flex-end',
+    marginRight: 30,
+    margin: 0
+  }
+});
