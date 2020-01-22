0be92ced20c89d8a44bdaaff1659b8e4a6d111f9
Alan Upstone <alan@helpfulconsultants.com>
2020-01-22 22:16:45 +0000

    remove log

--- api/user.js
+++ api/user.js
@@ -8,13 +8,13 @@ const dummyUrl =
   '?controller=api&action=checkUserId&eMail=' + eMailAddress + '&pin=' + pin;
 
 export const getUser = ({ email, pin }) => {
-  console.log('in user api - getUser called', email, pin);
+  //   console.log('in user api - getUser called', email, pin);
   const url =
     '?controller=api&action=checkUserId&eMail=' +
     email.toLowerCase() +
     '&pin=' +
     pin;
-  console.log(url);
+  //   console.log(url);
 
   return axios.get(url, {
     params: {
