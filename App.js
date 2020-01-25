3bec1d4d5116a516550a4616e7a3ceb3557114c2
Alan Upstone <alan@helpfulconsultants.com>
2020-01-25 15:14:31 +0000

    make status bar show on phone

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ export default function App(props) {
         <Provider store={store}>
           <PersistGate loading={<Loading />} persistor={persistor}>
             <View style={styles.container}>
-              {Platform.OS === 'ios' && <StatusBar barStyle='default' />}
+              {Platform.OS === 'ios' && <StatusBar barStyle='dark-content' />}
               <AppNavigator userIsSignedIn={userIsSignedIn} />
             </View>
           </PersistGate>
