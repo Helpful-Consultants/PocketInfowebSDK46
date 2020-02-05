fa26cf9d6e4f40e236b4538e51dd6c60e70c9d05
Alan Upstone <alan@helpfulconsultants.com>
2020-02-05 15:06:34 +0000

    change blinking view from  class to component

--- components/BlinkingView.js
+++ components/BlinkingView.js
@@ -1,57 +1,63 @@
-/*This is an Example of React Native Blinking Animation */
-import React, { Component } from 'react';
-//import react in our project
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { Icon, Text } from 'react-native-elements';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
-//import all the components we needed
 
-export default class BlinkingView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showItem: true };
+export default BlinkingView = props => {
+  const [showItem, setShowItem] = useState(true);
 
-    // Change the state every second or the time given by User.
-    setInterval(
-      () => {
-        this.setState(previousState => {
-          return { showItem: !previousState.showItem };
-        });
-      },
-      // Define blinking time in milliseconds
-      500
-    );
-  }
+  const {
+    blink,
+    colorOne,
+    colorTwo,
+    fallbackColor,
+    iconName,
+    iconSize,
+    iconType,
+    text
+  } = props;
 
-  render() {
-    // let display = this.state.showItem ? this.props.text : ' ';
-    // let color = this.state.showItem ? this.props.colorOne : this.props.colorTwo;
+  //   if (blink) {
+  //     setInterval(
+  //       setShowItem(!showItem),
+  //       // Define blinking time in milliseconds
+  //       500
+  //     );
+  //   }
+  //   useEffect(() => {
+  //     if (blink) {
+  //       setInterval(
+  //         setShowItem(!showItem),
+  //         // Define blinking time in milliseconds
+  //         500
+  //       );
+  //     }
+  //     return () => {
+  //       //   cleanup
+  //     };
+  //   }, [blink]);
 
-    return (
-      <View style={{ flexDirection: 'row' }}>
-        <Icon
-          name={this.props.iconName}
-          type={this.props.iconType}
-          size={this.props.iconSize}
-          color={
-            this.props.blink ? this.props.fallbackColor : this.props.colorOne
-          }
-        />
-        <Text> </Text>
-        <Text
-          style={{
-            paddingTop: RFPercentage(0.09),
-            fontSize: RFPercentage(2.3),
-            color: this.props.blink
-              ? this.state.showItem
-                ? this.props.colorOne
-                : this.props.colorTwo
-              : this.props.colorOne
-          }}
-        >
-          {this.props.text}
-        </Text>
-      </View>
-    );
-  }
-}
+  // let display = this.state.showItem ? this.props.text : ' ';
+  // let color = this.state.showItem ? this.props.colorOne : this.props.colorTwo;
+
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <Icon
+        name={iconName}
+        type={iconType}
+        size={iconSize}
+        color={blink ? fallbackColor : colorOne}
+      />
+      <Text> </Text>
+      <Text
+        style={{
+          paddingTop: RFPercentage(0.09),
+          fontSize: RFPercentage(2.3),
+          color: blink ? (showItem ? colorOne : colorTwo) : colorOne
+        }}
+      >
+        {text}
+      </Text>
+    </View>
+  );
+};
