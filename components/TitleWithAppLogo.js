9041844fd422f6a6b4bc555cda570b61a6a21d33
Alan Upstone <alan@helpfulconsultants.com>
2020-02-10 18:00:38 +0000

    change icon in title for android

--- components/TitleWithAppLogo.js
+++ components/TitleWithAppLogo.js
@@ -8,7 +8,11 @@ export default function getLogoTitle(props) {
   return (
     <View style={{ flexDirection: 'row', alignItems: 'center' }}>
       <Image
-        source={require('../assets/images/tiw-app-logo-trans.png')}
+        source={
+          Platform.OS === 'ios'
+            ? require('../assets/images/tiw-app-logo-trans.png')
+            : require('../assets/images/tiw-app-logo-trans-white.png')
+        }
         style={{ width: 26, height: 26 }}
       />
       <Text
