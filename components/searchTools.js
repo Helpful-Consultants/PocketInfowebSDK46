3c78c02f0880dfc803e93facea8b1ca489866cb8
Alan Upstone <alan@helpfulconsultants.com>
2020-02-14 16:00:19 +0000

    searchTools new rules

--- components/searchTools.js
+++ components/searchTools.js
@@ -13,6 +13,7 @@ export default searchTools = (
 
   const stringChecker = (rawString = '') => {
     // console.log('rawString ', rawString);
+
     if (rawString && rawString.length > 0) {
       const rawStringLowerCase = rawString.toLowerCase();
       const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
@@ -34,23 +35,23 @@ export default searchTools = (
             //   rawString,
             //   rawStringLowerCase.substring(3, 4)
             // );
-            return true;
+            return rawStringLowerCase.substring(0, 3);
           } else {
             // console.log(
             //   'rawString contains no digit ',
             //   rawString,
             //   rawStringLowerCase.substring(3, 4)
             // );
-            return false;
+            return null;
           }
         } else {
-          return false;
+          return null;
         }
       } else {
-        return false;
+        return null;
       }
     } else {
-      return false;
+      return null;
     }
   };
 
@@ -60,26 +61,48 @@ export default searchTools = (
     (searchString && searchString.toLowerCase()) || null;
   //   console.log('searchStringLowerCase ', searchStringLowerCase);
 
-  let searchStringSpaceStripped =
+  let searchStringSpacesStripped =
     (searchStringLowerCase && searchStringLowerCase.replace(/\s+/g, '')) ||
     null;
 
-  let isAseVagVasSearch =
-    searchStringSpaceStripped && searchStringSpaceStripped.length > 0
-      ? stringChecker(searchStringSpaceStripped)
-      : false;
+  //   let logIt = false;
+  //   console.log(searchString, searchStringSpacesStripped);
 
-  let searchStringSpaceStrippedWithoutAseVagVas = isAseVagVasSearch
-    ? searchStringSpaceStripped.substr(3)
+  //   if (searchStringSpacesStripped.includes('6558a')) {
+  //     logIt = true;
+  //   }
+
+  //   if (logIt) {
+  //     console.log('logIt', logIt, searchString, searchStringSpacesStripped);
+  //   }
+
+  let isAseVagVasSearch =
+    searchStringSpacesStripped && searchStringSpacesStripped.length > 0
+      ? stringChecker(searchStringSpacesStripped)
+      : null;
+
+  let searchStringSpacesStrippedWithoutAseVagVas = isAseVagVasSearch
+    ? searchStringSpacesStripped.substr(3)
     : searchStringLowerCase;
 
   //   console.log(
   //     'aseVagVas',
   //     searchStringLowerCase,
   //     isAseVagVasSearch,
-  //     searchStringSpaceStrippedWithoutAseVagVas
+  //     searchStringSpacesStrippedWithoutAseVagVas
   //   );
   //
+
+  //   if (logIt) {
+  //     console.log(
+  //       'logIt',
+  //       logIt,
+  //       searchString,
+  //       'isAseVagVasSearch',
+  //       isAseVagVasSearch,
+  //       searchStringSpacesStrippedWithoutAseVagVas
+  //     );
+  //   }
   if (itemList && itemList.length > 0) {
     // console.log('searchRecords, point 0');
     itemList.map(item => {
@@ -87,115 +110,179 @@ export default searchTools = (
 
       let isMatch = false;
 
-      if (item.loanToolNo) {
-        // loan tool
-        //   console.log('searchRecords, point 1', item.loanToolNo);
-
-        let itemLoanToolNumber =
-          (item.loanToolNo && item.loanToolNo.toLowerCase()) || '';
-        let itemToolNumber =
-          (item.supplierPartNo && item.supplierPartNo.toLowerCase()) || '';
-        let itemPartNumber =
-          (item.orderPartNo && item.orderPartNo.toLowerCase()) || '';
-        let itemDescription =
+      if (item.wipNumber) {
+        let itemSupplierPartNo =
+          (item.supplierPartNo && item.toolNumber.toLowerCase()) || '';
+        let itemOrderPartNo =
+          (item.orderPartNo && item.partNumber.toLowerCase()) || '';
+        let itemDesc =
           (item.toolDescription && item.toolDescription.toLowerCase()) || '';
 
-        let itemLoanToolNumberSpaceStripped =
-          itemLoanToolNumber && itemLoanToolNumber.replace(/\s+/g, '');
-        let itemToolNumberSpaceStripped =
-          itemToolNumber && itemToolNumber.replace(/\s+/g, '');
-        let itemPartNumberSpaceStripped =
-          itemPartNumber && itemPartNumber.replace(/\s+/g, '');
-        let itemSpaceStrippedDescription =
-          itemDescription && itemDescription.replace(/\s+/g, '');
+        let itemSupplierPartNoSpacesStripped =
+          itemSupplierPartNo && itemSupplierPartNo.replace(/\s+/g, '');
+        let itemOrderPartNoSpacesStripped =
+          itemOrderPartNo && itemOrderPartNo.replace(/\s+/g, '');
+        let itemDescSpacesStripped = itemDesc && itemDesc.replace(/\s+/g, '');
+        // It's a wip!
+      } else if (item.loanToolNo) {
+        // loan tool
+        //   console.log('searchRecords, point 1', item.loanToolNo);
+        // if (logIt) console.log(searchString, 'point a loan tool');
+        let itemLoanToolNo =
+          (item.loanToolNo && item.loanToolNo.toLowerCase()) || '';
+        let itemSupplierPartNo =
+          (item.supplierPartNo && item.supplierPartNo.toLowerCase()) || '';
+        let itemOrderPartNo =
+          (item.orderPartNo && item.orderPartNo.toLowerCase()) || '';
+        let itemDesc =
+          (item.toolDescription && item.toolDescription.toLowerCase()) || '';
 
-        //   console.log('itemToolNumber', itemToolNumber);
-        //   console.log('itemPartNumber', itemPartNumber);
-        //   console.log('itemDesc', itemDescription);
-        //   console.log('itemToolNumberSpaceStripped', itemToolNumberSpaceStripped);
-        //   console.log('itemPartNumberSpaceStripped', itemPartNumberSpaceStripped);
-        //   console.log('itemSpaceStrippedDesc', itemSpaceStrippedDescription);
+        let itemLoanToolNoSpacesStripped =
+          itemLoanToolNo && itemLoanToolNo.replace(/\s+/g, '');
+        let itemSupplierPartNoSpacesStripped =
+          itemSupplierPartNo && itemSupplierPartNo.replace(/\s+/g, '');
+        let itemOrderPartNoSpacesStripped =
+          itemOrderPartNo && itemOrderPartNo.replace(/\s+/g, '');
+        let itemDescSpacesStripped = itemDesc && itemDesc.replace(/\s+/g, '');
+
+        //   console.log('itemSupplierPartNo', itemSupplierPartNo);
+        //   console.log('itemOrderPartNo', itemOrderPartNo);
+        //   console.log('itemDesc', itemDesc);
+        //   console.log('itemSupplierPartNoSpacesStripped', itemSupplierPartNoSpacesStripped);
+        //   console.log('itemOrderPartNoSpacesStripped', itemOrderPartNoSpacesStripped);
+        //   console.log('itemSpacesStrippedDesc', itemSpacesStrippedDescription);
         if (isAseVagVasSearch) {
+          //   if (logIt) console.log(searchString, 'point d');
           if (
-            (itemToolNumberSpaceStripped &&
-              itemToolNumberSpaceStripped.includes(
-                searchStringSpaceStripped
-              )) ||
-            (itemPartNumberSpaceStripped &&
-              itemPartNumberSpaceStripped.includes(
-                searchStringSpaceStripped
-              )) ||
-            (itemDescription &&
-              itemDescription.includes(
-                searchStringSpaceStrippedWithoutAseVagVas
-              ))
+            itemDescSpacesStripped &&
+            itemDescSpacesStripped.includes(searchStringSpacesStripped)
           ) {
-            // console.log('searchRecords, point 2', item.toolNumber);
+            // console.log('searchRecords, point 2', item.toolNo);
             isMatch = true;
+          } else {
+            // if (logIt) console.log(searchString, 'point ce');
+            if (isAseVagVasSearch === 'ase') {
+              //   if (logIt) console.log(searchString, 'point ase');
+              if (
+                itemOrderPartNoSpacesStripped &&
+                itemOrderPartNoSpacesStripped.includes(
+                  searchStringSpacesStripped
+                )
+              ) {
+                // console.log('searchRecords, point 2', item.toolNo);
+                isMatch = true;
+              }
+            } else {
+              //   if (logIt) console.log(searchString, 'point vag vas');
+              if (
+                itemSupplierPartNoSpacesStripped &&
+                itemSupplierPartNoSpacesStripped.includes(
+                  searchStringSpacesStrippedWithoutAseVagVas
+                )
+              ) {
+                // console.log('searchRecords, point 2', item.toolNo);
+                isMatch = true;
+              }
+            }
           }
         } else if (
-          (itemLoanToolNumberSpaceStripped &&
-            itemLoanToolNumberSpaceStripped.includes(
-              searchStringSpaceStripped
+          (itemLoanToolNoSpacesStripped &&
+            itemLoanToolNoSpacesStripped.includes(
+              searchStringSpacesStripped
             )) ||
-          (itemToolNumberSpaceStripped &&
-            itemToolNumberSpaceStripped.includes(searchStringSpaceStripped)) ||
-          (itemPartNumberSpaceStripped &&
-            itemPartNumberSpaceStripped.includes(searchStringSpaceStripped)) ||
-          (itemDescription && itemDescription.includes(searchStringLowerCase))
+          (itemSupplierPartNoSpacesStripped &&
+            itemSupplierPartNoSpacesStripped.includes(
+              searchStringSpacesStripped
+            )) ||
+          (itemOrderPartNoSpacesStripped &&
+            itemOrderPartNoSpacesStripped.includes(
+              searchStringSpacesStripped
+            )) ||
+          (itemDesc && itemDesc.includes(searchStringLowerCase))
         ) {
-          // console.log('searchRecords, point 2', item.toolNumber);
+          // console.log('searchRecords, point 2', item.toolNo);
           isMatch = true;
           // dealer tool}
         }
       } else if (item.toolNumber) {
-        let itemToolNumber =
+        // dealer tool or equip
+
+        let itemToolNo =
           (item.toolNumber && item.toolNumber.toLowerCase()) || '';
-        let itemPartNumber =
+        let itemPartNo =
           (item.partNumber && item.partNumber.toLowerCase()) || '';
-        let itemDescription =
+        let itemDesc =
           (item.partDescription && item.partDescription.toLowerCase()) || '';
 
-        let itemToolNumberSpaceStripped =
-          itemToolNumber && itemToolNumber.replace(/\s+/g, '');
-        let itemPartNumberSpaceStripped =
-          itemPartNumber && itemPartNumber.replace(/\s+/g, '');
-        // let itemSpaceStrippedDescription =
-        //   itemDescription && itemDescription.replace(/\s+/g, '');
+        let itemToolNoSpacesStripped =
+          itemToolNo && itemToolNo.replace(/\s+/g, '');
+        let itemPartNoSpacesStripped =
+          itemPartNo && itemPartNo.replace(/\s+/g, '');
+        let itemDescSpacesStripped = itemDesc && itemDesc.replace(/\s+/g, '');
 
-        //   console.log('itemToolNumber', itemToolNumber);
-        //   console.log('itemPartNumber', itemPartNumber);
-        //   console.log('itemDesc', itemDescription);
-        //   console.log('itemToolNumberSpaceStripped', itemToolNumberSpaceStripped);
-        //   console.log('itemPartNumberSpaceStripped', itemPartNumberSpaceStripped);
-        //   console.log('itemSpaceStrippedDesc', itemSpaceStrippedDescription);
+        // if (item.id && item.id === '5997') {
+        //   console.log(item);
+        // }
+        //   console.log('itemToolNo', itemToolNo);
+        //   console.log('itemPartNo', itemPartNo);
+        //   console.log('itemDesc', itemDesc);
+        //   console.log('itemToolNoSpacesStripped', itemToolNoSpacesStripped);
+        //   console.log('itemPartNoSpacesStripped', itemPartNoSpacesStripped);
+        //   console.log('itemSpacesStrippedDesc', itemSpacesStrippedDescription);
+        // if (logIt) console.log(searchString, 'point c');
 
-        if (isAseVagVasSearch) {
+        // ASE/VAG/VAS don't apply to tools, just equip
+        if (
+          isAseVagVasSearch &&
+          item.toolType &&
+          (item.toolType === 'Equipment' || item.toolType === 'equipment')
+        ) {
+          //   if (logIt) console.log(searchString, 'point d');
           if (
-            (itemToolNumberSpaceStripped &&
-              itemToolNumberSpaceStripped.includes(
-                searchStringSpaceStripped
-              )) ||
-            (itemPartNumberSpaceStripped &&
-              itemPartNumberSpaceStripped.includes(
-                searchStringSpaceStripped
-              )) ||
-            (itemDescription &&
-              itemDescription.includes(
-                searchStringSpaceStrippedWithoutAseVagVas
-              ))
+            itemDescSpacesStripped &&
+            itemDescSpacesStripped.includes(searchStringSpacesStripped)
           ) {
-            // console.log('searchRecords, point 2', item.toolNumber);
+            // console.log('searchRecords, point 2', item.toolNo);
             isMatch = true;
+          } else {
+            // if (logIt) console.log(searchString, 'point ce');
+            if (isAseVagVasSearch === 'ase') {
+              //   if (logIt) console.log(searchString, 'point ase');
+              if (
+                itemPartNoSpacesStripped &&
+                itemPartNoSpacesStripped.includes(
+                  searchStringSpacesStrippedWithoutAseVagVas
+                )
+              ) {
+                // console.log('searchRecords, point 2', item.toolNo);
+                isMatch = true;
+              }
+            } else {
+              //   if (logIt) console.log(searchString, 'point vag vas');
+              if (
+                itemToolNoSpacesStripped &&
+                itemToolNoSpacesStripped.includes(
+                  searchStringSpacesStrippedWithoutAseVagVas
+                )
+              ) {
+                // console.log('searchRecords, point 2', item.toolNo);
+                isMatch = true;
+              }
+            }
           }
         } else if (
-          (itemToolNumberSpaceStripped &&
-            itemToolNumberSpaceStripped.includes(searchStringSpaceStripped)) ||
-          (itemPartNumberSpaceStripped &&
-            itemPartNumberSpaceStripped.includes(searchStringSpaceStripped)) ||
-          (itemDescription && itemDescription.includes(searchStringLowerCase))
+          // search part no for both tools & equip
+          (itemPartNoSpacesStripped &&
+            itemPartNoSpacesStripped.includes(searchStringSpacesStripped)) ||
+          // only search tool no for equip
+          (item.toolType &&
+            (item.toolType === 'Equipment' || item.toolType === 'equipment') &&
+            itemToolNoSpacesStripped &&
+            itemToolNoSpacesStripped.includes(searchStringSpacesStripped)) ||
+          // search desc for both tools & equip
+          (itemDesc && itemDesc.includes(searchStringLowerCase))
         ) {
-          //   console.log('searchRecords, point 3', item.toolNumber);
+          //   console.log('searchRecords, point 3', item.toolNo);
           isMatch = true;
         }
       }
@@ -203,6 +290,7 @@ export default searchTools = (
       if (isMatch) {
         //   console.log('*******searchRecords, point 5', isMatch, item);
         newFilteredItems.push(item);
+        // if (logIt) console.log(searchString, 'match');
       }
     });
     // console.log('££££££££££newFilteredItems', newFilteredItems.length);
