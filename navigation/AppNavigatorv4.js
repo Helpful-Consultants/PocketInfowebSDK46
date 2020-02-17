dfc2e2f28fa771d3d872bd0cce73b2c9682ce120
Alan Upstone <alan@helpfulconsultants.com>
2020-02-17 18:22:41 +0000

    store v4 navigators

---
+++ navigation/AppNavigatorv4.js
@@ -0,0 +1,223 @@
+import React from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
+
+// import {
+//   createDrawerNavigator,
+//   DrawerNavigatorItems as DrawerItems
+// } from 'react-navigation-drawer';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+// import {
+//   getBrand,
+//   getVersion,
+//   getManufacturer
+// } from 'react-native-device-info';
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
+import SignInScreen from '../screens/SignInScreen';
+import SignOutScreen from '../screens/SignOutScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import ForgottenPasswordScreen from '../screens/ForgottenPasswordScreen';
+import HomeScreen from '../screens/HomeScreen';
+import AppInfo from '../components/AppInfo';
+// import StatsScreen from '../screens/StatsScreen';
+// import OdisScreen from '../screens/OdisScreen';
+import WipTabNavigator from './WipTabNavigator';
+import NewsTabNavigator from './NewsTabNavigator';
+
+// console.log(Constants && Constants);
+// console.log(Platform && Platform);
+
+const SignedOutStack = createStackNavigator({
+  SignIn: {
+    screen: SignInScreen,
+    navigationOptions: {
+      title: 'Sign In'
+    },
+    navigationOptions: () => ({
+      headerShown: false
+    })
+  },
+  ForgottenPassword: {
+    screen: ForgottenPasswordScreen,
+    navigationOptions: {
+      title: 'Forgotten Password'
+    },
+    navigationOptions: () => ({
+      headerShown: true
+    })
+  },
+  Register: {
+    screen: RegisterScreen,
+    navigationOptions: {
+      title: 'Register'
+    },
+    navigationOptions: () => ({
+      headerShown: true
+    })
+  },
+  SignOut: {
+    screen: SignOutScreen,
+    navigationOptions: {
+      title: 'Sign out'
+    }
+  },
+  navigationOptions: () => ({
+    headerShown: false
+  })
+});
+
+// Stats stack
+// const StatsStack = createStackNavigator({
+//   Stats: StatsScreen
+// });
+
+// // OdisStack.navigationOptions = {
+// //   tabBarLabel: 'ODIS',
+// //   tabBarIcon: ({ focused }) => (
+// //     <TabBarIcon
+// //       focused={focused}
+// //       name={Platform.OS === 'ios' ? 'ios-tv' : 'md-tv'}
+// //     />
+// //   )
+// // };
+
+// OdisStack.path = '';
+// End ODIS screen
+
+// const AppDrawerNavigator = createDrawerNavigator({
+//   'Quick Start': {
+//     screen: HomeScreen
+//   },
+//   'Main menu': {
+//     screen: WipTabNavigator
+//   },
+//   'News menu': {
+//     screen: NewsTabNavigator
+//   },
+//   'Odis versions': {
+//     screen: OdisScreen
+//   },
+//   Stats: {
+//     screen: StatsScreen
+//   },
+//   'Sign out': {
+//     screen: SignInScreen
+//   }
+// });
+
+const DrawerContent = props => {
+  return (
+    <ScrollView style={styles.container}>
+      <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+        <View style={styles.stretched}>
+          <DrawerItems {...props} />
+          <AppInfo />
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+const AppDrawerNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: { drawerLabel: 'Home' }
+    },
+    FindToolsStack: {
+      screen: WipTabNavigator,
+      navigationOptions: {
+        drawerLabel: 'Find tools, return tools, jobs & LTP',
+        initialRouteName: 'FindTools'
+      }
+    },
+    NewsStack: {
+      screen: NewsTabNavigator,
+      navigationOptions: {
+        drawerLabel: 'News, products, ODIS, stats',
+        initialRouteName: 'News'
+      }
+    },
+
+    SignedOutStack: {
+      screen: SignOutScreen,
+      navigationOptions: {
+        drawerLabel: 'Sign out'
+      }
+    }
+  },
+  {
+    contentComponent: DrawerContent
+  }
+);
+
+// Tab navigator
+// const MainTabNavigator = createBottomTabNavigator({
+//   Home: HomeScreen,
+//   Wip: WipTabNavigator,
+//   News: NewsTabNavigator
+// });
+
+// MainTabNavigator.path = '';
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      AuthLoading: AuthLoadingScreen,
+      Auth: SignedOutStack,
+      Main: AppDrawerNavigator
+      //   Main: CustomDrawerContentComponent
+    },
+    // { initialRouteName: userIsSignedIn ? 'Main' : 'Auth' }
+    { initialRouteName: 'AuthLoading' }
+  )
+);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  zzstretched: {
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'space-between'
+    // backgroundColor: 'red'
+  },
+  footerContainer: {
+    // flex: 1,
+    // flexDirection: 'column',
+    // alignItems: 'flex-start',
+    // justifyContent: 'flex-end',
+    // backgroundColor: 'red'
+  },
+  appName: {
+    paddingTop: 100,
+    paddingLeft: 18,
+    fontFamily: 'the-sans-bold',
+    fontSize: RFPercentage(2.2)
+    // fontStyle: 'italic'
+  },
+  brand: {
+    paddingTop: 5,
+    paddingLeft: 18,
+    fontFamily: 'the-sans-bold',
+    fontSize: RFPercentage(1.9)
+    // fontStyle: 'italic'
+  },
+  appVersion: {
+    paddingTop: 5,
+    paddingLeft: 18,
+    fontFamily: 'the-sans',
+    fontSize: RFPercentage(1.8)
+    // fontStyle: 'italic'
+  },
+  deviceVersion: {
+    paddingTop: 15,
+    paddingLeft: 18,
+    fontFamily: 'the-sans',
+    fontSize: RFPercentage(1.8)
+    // fontStyle: 'italic'
+  }
+});
---
+++ navigation/WipTabNavigatorv4.js
@@ -0,0 +1,205 @@
+import React from 'react';
+import { Platform, StyleSheet, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+// import NewsScreen from '../screens/NewsScreen';
+// import ProductsScreen from '../screens/ProductsScreen';
+
+import FindToolsScreen from '../screens/FindToolsScreen';
+import LtpScreen from '../screens/LtpScreen';
+import JobsScreen from '../screens/JobsScreen';
+import BookedOutToolsScreen from '../screens/BookedOutToolsScreen';
+import Colors from '../constants/Colors';
+
+// const config = Platform.select({
+//   web: { headerMode: 'screen' },
+//   default: {}
+// });
+
+// Home screen
+const HomeStack = createStackNavigator({
+  Home: HomeScreen
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: ({ focused }) => (
+    <Text style={focused ? styles.focused : styles.notFocused}>Home</Text>
+  ),
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-home' : 'md-home'}
+    />
+  )
+};
+// End Home screen
+
+// Find Tools screen
+const FindToolsStack = createStackNavigator({
+  FindTools: FindToolsScreen
+});
+
+FindToolsStack.navigationOptions = {
+  tabBarColor: Colors.vwgWhite,
+  tabBarLabel:
+    Platform.OS === 'ios'
+      ? ({ focused }) => (
+          <Text style={focused ? styles.focused : styles.notFocused}>
+            Find tools
+          </Text>
+        )
+      : 'Find tools',
+
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-build' : 'md-build'}
+    />
+  )
+};
+// End Find Tools screen
+
+// BookedOutTools screen
+const BookedOutToolsStack = createStackNavigator({
+  BookedOutTools: BookedOutToolsScreen
+});
+
+BookedOutToolsStack.navigationOptions = {
+  tabBarColor: Colors.vwgWhite,
+  tabBarLabel:
+    Platform.OS === 'ios'
+      ? ({ focused }) => (
+          <Text style={focused ? styles.focused : styles.notFocused}>
+            Booked tools
+          </Text>
+        )
+      : 'Booked tools',
+
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-return-left' : 'md-return-left'}
+    />
+  )
+};
+// End Return Tools screen
+
+// Jobs screen
+const JobsStack = createStackNavigator({
+  Jobs: JobsScreen
+});
+
+JobsStack.navigationOptions = {
+  tabBarColor: Colors.vwgWhite,
+  tabBarLabel:
+    Platform.OS === 'ios'
+      ? ({ focused }) => (
+          <Text style={focused ? styles.focused : styles.notFocused}>Jobs</Text>
+        )
+      : 'Jobs',
+
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-clipboard' : 'md-today'}
+    />
+  )
+};
+// End Jobs screen
+
+// LTP screen
+const LtpStack = createStackNavigator({
+  Ltp: LtpScreen
+});
+
+LtpStack.navigationOptions = {
+  tabBarColor: Colors.vwgWhite,
+  tabBarLabel:
+    Platform.OS === 'ios'
+      ? ({ focused }) => (
+          <Text style={focused ? styles.focused : styles.notFocused}>LTP</Text>
+        )
+      : 'LTP',
+
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-swap' : 'md-swap'}
+    />
+  )
+};
+// End LTP screen
+
+// Tab navigator
+const WipTabNavigator =
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(
+        {
+          FindToolsStack,
+          BookedOutToolsStack,
+          JobsStack,
+          LtpStack
+        },
+        {
+          labeled: true,
+          shifting: false,
+          backBehavior: 'history',
+          activeColor: Colors.vwgDeepBlue,
+          inactiveColor: Colors.vwgLink,
+          tabBarColor: Colors.vwgWhite,
+          barStyle: {
+            labelPosition: 'below-icon',
+
+            // height: RFPercentage(6.4),
+            backgroundColor: Colors.vwgWhite
+          }
+          //   tabBarOptions: {
+          //     labelPosition: 'below-icon',
+          //     style: {
+          //       height: RFPercentage(6.4)
+          //     }
+          //   },
+          //   tabStyle: {
+          //     height: RFPercentage(2.2)
+          //   }
+        }
+      )
+    : createBottomTabNavigator(
+        {
+          FindToolsStack,
+          BookedOutToolsStack,
+          JobsStack,
+          LtpStack
+        },
+        {
+          tabBarOptions: {
+            labelPosition: 'below-icon',
+            style: {
+              height: RFPercentage(6.4)
+            }
+          },
+          tabStyle: {
+            height: RFPercentage(2.2)
+          }
+        }
+      );
+// End Tab navigator
+
+const styles = StyleSheet.create({
+  focused: {
+    fontFamily: 'the-sans',
+    color: Colors.vwgActiveLink,
+    fontSize: RFPercentage(1.7)
+  },
+  notFocused: {
+    fontFamily: 'the-sans',
+    color: Colors.vwgInactiveLink,
+    fontSize: RFPercentage(1.7)
+  }
+});
+
+export default WipTabNavigator;
