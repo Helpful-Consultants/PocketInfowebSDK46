7ef384a06335e1ec9933c645331dcc0525482029
Alan Upstone <alan@helpfulconsultants.com>
2020-02-17 18:23:19 +0000

    signedout stack in own file

---
+++ navigation/SignedOutStack.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignInScreen, {
+  screenOptions as SignInScreenOptions
+} from '../screens/SignInScreen';
+import SignOutScreen, {
+  screenOptions as SignOutScreenOptions
+} from '../screens/SignOutScreen';
+import RegisterScreen, {
+  screenOptions as RegisterScreenOptions
+} from '../screens/RegisterScreen';
+import ForgottenPasswordScreen, {
+  screenOptions as ForgottenPasswordScreenOptions
+} from '../screens/ForgottenPasswordScreen';
+
+const defaultStackNavOptions = {
+  headerShown: false,
+  cardStyle: { backgroundColor: 'white' }
+};
+
+const Stack = createStackNavigator();
+
+export default SignedOutStack = () => {
+  return (
+    <Stack.Navigator screenOptions={defaultStackNavOptions}>
+      <Stack.Screen
+        name={'SignIn'}
+        component={SignInScreen}
+        options={SignInScreenOptions}
+      />
+      <Stack.Screen
+        name={'ForgottenPassword'}
+        component={ForgottenPasswordScreen}
+        options={ForgottenPasswordScreenOptions}
+      />
+      <Stack.Screen
+        name={'Register'}
+        component={RegisterScreen}
+        options={RegisterScreenOptions}
+      />
+      <Stack.Screen
+        name={'SignOut'}
+        component={SignOutScreen}
+        options={SignOutScreenOptions}
+      />
+    </Stack.Navigator>
+  );
+};
