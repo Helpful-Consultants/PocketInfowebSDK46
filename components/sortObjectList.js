d325e9901d23d67bd9fc85d889d418671cb9b9ea
Alan Upstone <alan@helpfulconsultants.com>
2020-02-23 11:15:53 +0000

    new sortObjectList.js

---
+++ components/sortObjectList.js
@@ -0,0 +1,13 @@
+const compareObjectValues = (key, order = 'asc') => {
+  return function innerSort(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
+    const comparison = a[key].localeCompare(b[key]);
+
+    return order === 'desc' ? comparison * -1 : comparison;
+  };
+};
+
+export default sortObjectList = (objArr, key, order = 'asc') => {
+  console.log(objArr.length, key, order);
+  return objArr.sort(compareObjectValues(key, order));
+};
