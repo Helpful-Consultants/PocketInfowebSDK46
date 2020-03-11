1e3eaf198dda38589b199730cda62af9ba9af430
Alan Upstone <alan@helpfulconsultants.com>
2020-03-11 18:02:58 +0000

    don't try fetching wips if fetch data obj not all there

--- sagas/dealerWips.js
+++ sagas/dealerWips.js
@@ -15,157 +15,164 @@ import * as api from '../api/dealerWips';
 // Get WIPS
 function* getDealerWips({ payload }) {
   //   console.log('in saga get dealerWips, payload', payload && payload);
-  let statusCode = null;
-  let errorText = 'A server error occurred when trying to get the dealer jobs';
-  let dataErrorUrl = null;
-  yield put(actions.getDealerWipsStart());
-  try {
-    const result = yield call(api.getDealerWips, {
-      dealerId: payload.dealerId,
-      intId: payload.intId
-    });
-    // console.log('in saga get dealerWips -200');
+  if (payload.dealerId && payload.intId) {
+    let statusCode = null;
+    let errorText =
+      'A server error occurred when trying to get the dealer jobs';
+    let dataErrorUrl = null;
 
-    // console.log('end results in saga get dealerWips, success');
-    // console.log('@@@@@@@@@@@result starts');
-    // console.log(result);
-    // console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
-    // console.log(
-    //   'the wips data starts here',
-    //   result.data && result.data,
-    //   'wips data ends here'
-    // );
-    // console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
-    if (
-      result.data &&
-      result.data.length > 0 &&
-      result.data[0] &&
-      result.data[0].id &&
-      //   result.data[0].tools &&
-      result.data[0].wipNumber
-    ) {
-      //   console.log('in wips saga - good 200');
+    yield put(actions.getDealerWipsStart());
+    try {
+      const result = yield call(api.getDealerWips, {
+        dealerId: payload.dealerId,
+        intId: payload.intId
+      });
+      // console.log('in saga get dealerWips -200');
 
-      //   console.log('in Wips saga - good 200');
-      //   console.log(result.data);
-      yield put(
-        actions.getDealerWipsSuccess({
-          items: result.data,
-          statusCode:
-            (result.status && result.status) ||
-            (result.request.status && result.request.status) ||
-            null
-        })
-      );
-    } else if (result && result.data && result.data.length > 0) {
-      console.log(
-        'in Wips saga - an empty 200',
-        result.request.status && result.request.status,
-        payload && payload
-      );
-      //   console.log(result && result);
-      yield put(
-        actions.getDealerWipsSuccess({
-          items: [],
-          statusCode:
-            (result.status && result.status) ||
-            (result.request.status && result.request.status) ||
-            null
-        })
-      );
-    } else {
-      console.log('dealer wips weird result');
-      //   console.log(result && result);
-      yield put(
-        actions.dealerWipsError({
-          error:
-            (result.request.response && result.request.response.toString()) ||
-            'An error occurred when trying to update the jobs',
-          statusCode: (result.request.status && result.request.status) || null,
-          dataErrorUrl:
-            (result && result.responseURL && result.responseURL) ||
-            (result &&
-              result.request &&
-              result.request._url &&
-              result.request._url) ||
-            null
-        })
-      );
-    }
+      // console.log('end results in saga get dealerWips, success');
+      // console.log('@@@@@@@@@@@result starts');
+      // console.log(result);
+      // console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
+      // console.log(
+      //   'the wips data starts here',
+      //   result.data && result.data,
+      //   'wips data ends here'
+      // );
+      // console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
+      if (
+        result.data &&
+        result.data.length > 0 &&
+        result.data[0] &&
+        result.data[0].id &&
+        //   result.data[0].tools &&
+        result.data[0].wipNumber
+      ) {
+        //   console.log('in wips saga - good 200');
 
-    // console.log(result);
-    // console.log('end results in saga get dealerWips, success');
-  } catch (error) {
-    // console.log('server error in saga get dealerWips !!!!!!!!!!!!!!');
-    // console.log('whole Error', error);
-    // console.log('whole Error ends');
-    // console.log(error && error.config);
+        //   console.log('in Wips saga - good 200');
+        //   console.log(result.data);
+        yield put(
+          actions.getDealerWipsSuccess({
+            items: result.data,
+            statusCode:
+              (result.status && result.status) ||
+              (result.request.status && result.request.status) ||
+              null
+          })
+        );
+      } else if (result && result.data && result.data.length > 0) {
+        console.log(
+          'in Wips saga - an empty 200',
+          result.request.status && result.request.status,
+          payload && payload
+        );
+        //   console.log(result && result);
+        yield put(
+          actions.getDealerWipsSuccess({
+            items: [],
+            statusCode:
+              (result.status && result.status) ||
+              (result.request.status && result.request.status) ||
+              null
+          })
+        );
+      } else {
+        console.log('dealer wips weird result');
+        //   console.log(result && result);
+        yield put(
+          actions.dealerWipsError({
+            error:
+              (result.request.response && result.request.response.toString()) ||
+              'An error occurred when trying to update the jobs',
+            statusCode:
+              (result.request.status && result.request.status) || null,
+            dataErrorUrl:
+              (result && result.responseURL && result.responseURL) ||
+              (result &&
+                result.request &&
+                result.request._url &&
+                result.request._url) ||
+              null
+          })
+        );
+      }
 
-    if (error.response) {
-      //   console.log('error response starts');
-      //   console.log('error response', error.response);
-      //   console.log('error response ends');
-      if (error.response.status) {
-        statusCode = error.response.status;
-      }
-      if (error.response.data) {
-        errorText = error.response.data;
-      }
-      if (error.response.config.url) {
-        dataErrorUrl = error.response.config.url;
-      }
-      // console.error(error);if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      //   console.log('error.response.data', error.response.data);
-      // console.log(error.response.status);
-      // console.log(error.response.headers);
-    } else if (error.request) {
-      //   console.log('error request start');
-      //   console.log('error request', error.request);
-      //   console.log('error request ends');
-      if (error.request.status) {
-        statusCode = error.request.status;
-      }
-      if (error.request._response) {
-        errorText = error.request._response;
-        if (
-          error.request._response.indexOf('connect') !== -1 ||
-          error.request._response.indexOf('timed out') !== -1
-        ) {
-          statusCode = 408;
-        } else {
-          if (error.request.status) {
-            statusCode = error.request.status;
+      // console.log(result);
+      // console.log('end results in saga get dealerWips, success');
+    } catch (error) {
+      // console.log('server error in saga get dealerWips !!!!!!!!!!!!!!');
+      // console.log('whole Error', error);
+      // console.log('whole Error ends');
+      // console.log(error && error.config);
+
+      if (error.response) {
+        //   console.log('error response starts');
+        //   console.log('error response', error.response);
+        //   console.log('error response ends');
+        if (error.response.status) {
+          statusCode = error.response.status;
+        }
+        if (error.response.data) {
+          errorText = error.response.data;
+        }
+        if (error.response.config.url) {
+          dataErrorUrl = error.response.config.url;
+        }
+        // console.error(error);if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        //   console.log('error.response.data', error.response.data);
+        // console.log(error.response.status);
+        // console.log(error.response.headers);
+      } else if (error.request) {
+        //   console.log('error request start');
+        //   console.log('error request', error.request);
+        //   console.log('error request ends');
+        if (error.request.status) {
+          statusCode = error.request.status;
+        }
+        if (error.request._response) {
+          errorText = error.request._response;
+          if (
+            error.request._response.indexOf('connect') !== -1 ||
+            error.request._response.indexOf('timed out') !== -1
+          ) {
+            statusCode = 408;
+          } else {
+            if (error.request.status) {
+              statusCode = error.request.status;
+            }
           }
         }
+        if (error.request.responseURL) {
+          dataErrorUrl = error.request.responseURL;
+        } else if (error.request._url) {
+          dataErrorUrl = error.request._url;
+        }
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        //   console.log('error.request'), error.request;
+      } else if (error.message) {
+        // Something happened in setting up the request that triggered an Error
+        //   console.log('error message starts');
+        //   console.log('Error message', error.message);
+        //   console.log('error message ends');
+        if (error.message.indexOf(' 500') !== -1) {
+          statusCode = 500;
+        }
+        errorText = error.message;
       }
-      if (error.request.responseURL) {
-        dataErrorUrl = error.request.responseURL;
-      } else if (error.request._url) {
-        dataErrorUrl = error.request._url;
-      }
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      //   console.log('error.request'), error.request;
-    } else if (error.message) {
-      // Something happened in setting up the request that triggered an Error
-      //   console.log('error message starts');
-      //   console.log('Error message', error.message);
-      //   console.log('error message ends');
-      if (error.message.indexOf(' 500') !== -1) {
-        statusCode = 500;
-      }
-      errorText = error.message;
+      yield put(
+        actions.dealerWipsError({
+          error: errorText,
+          statusCode: statusCode,
+          dataErrorUrl: dataErrorUrl
+        })
+      );
     }
-    yield put(
-      actions.dealerWipsError({
-        error: errorText,
-        statusCode: statusCode,
-        dataErrorUrl: dataErrorUrl
-      })
-    );
+  } else {
+    console.log('not getting wips');
   }
 }
 
