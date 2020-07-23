7dc049dab3075867df4c43529d65664e68d8a93f
Alan Upstone <alan@helpfulconsultants.com>
2020-07-23 17:51:56 +0100

    new component getBaseStyles

---
+++ components/getActiveDimensions.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import { useWindowDimensions } from 'react-native';
+// import { Dimensions } from 'react-native';
+// const windowWidth = useWindowDimensions().width;
+// const windowHeight = useWindowDimensions().height;
+
+// const { height, width } = Dimensions.get('window');
+
+export default getActiveDimensions = (props) => {
+  //   const windowWidth = useWindowDimensions().width;
+  //   const windowHeight = useWindowDimensions().height;
+  console.log(props && 'in getActiveDimensions', props && props);
+};
