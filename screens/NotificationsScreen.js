808a898896d1c3c5372cd8ffc7769b3ef631c130
Alan Upstone <alan@helpfulconsultants.com>
2021-06-28 17:38:54 +0100

    new notifications screen

--- screens/NotificationsScreen.js
+++ screens/NotificationsScreen.js
@@ -1,7 +1,15 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Platform, Text, useWindowDimensions, View } from 'react-native';
+import {
+  Platform,
+  ScrollView,
+  SectionList,
+  Text,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
 // import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import TitleWithAppLogo from '../components/TitleWithAppLogo';
 import TabBarIcon from '../components/TabBarIcon';
@@ -11,10 +19,17 @@ import ErrorDetails from '../components/ErrorDetails';
 // import BadgedTabBarText from '../components/BadgedTabBarText';
 import { revalidateUserCredentials } from '../actions/user';
 import { getCalibrationExpiryRequest } from '../actions/calibrationExpiry';
+import { getServiceMeasuresRequest } from '../actions/serviceMeasures';
+import { getLtpLoansRequest } from '../actions/ltpLoans';
 import CalibrationExpiryList from './CalibrationExpiryList';
 import calibrationExpiryDummyData from '../dummyData/calibrationExpiryDummyData.js';
+import LtpLoansList from './LtpLoansList';
+import ltpLoansDummyData from '../dummyData/ltpLoansDummyData.js';
+import ServiceMeasuresList from './ServiceMeasuresList';
+import serviceMeasuresDummyData from '../dummyData/serviceMeasuresDummyData.js';
 
 const demoModeOn = true;
+const now = moment();
 
 export default NotificationsScreen = (props) => {
   const windowDim = useWindowDimensions();
@@ -22,14 +37,52 @@ export default NotificationsScreen = (props) => {
   const calibrationExpiryItems = useSelector(
     (state) => state.calibrationExpiry.calibrationExpiryItems
   );
+  const ltpLoansItems = useSelector((state) => state.ltpLoans.ltpLoansItems);
+  const serviceMeasuresItems = useSelector(
+    (state) => state.serviceMeasures.serviceMeasuresItems
+  );
   const userIsValidated = useSelector((state) => state.user.userIsValidated);
   const userDataObj = useSelector((state) => state.user.userData[0]);
   const dealerId = userDataObj && userDataObj.dealerId;
   const userIntId = userDataObj && userDataObj.intId.toString();
-  const isLoading = useSelector((state) => state.stats.isLoading);
-  const dataError = useSelector((state) => state.stats.error);
-  const dataStatusCode = useSelector((state) => state.odis.statusCode);
-  const dataErrorUrl = useSelector((state) => state.odis.dataErrorUrl);
+  const [isLoadingAny, setIsLoadingAny] = useState(false);
+  const [dataNameInPlay, setDataNameInPlay] = useState('');
+  const [dataErrorAny, setDataErrorAny] = useState('');
+  const [dataStatusCodeAny, setDataStatusCodeAny] = useState('');
+  const [dataErrorUrlAny, setDataErrorUrlAny] = useState('');
+  const [dataErrorSummary, setDataErrorSummary] = useState('');
+  const isLoadingCalibrationExpiry = useSelector(
+    (state) => state.calibrationExpiry.isLoading
+  );
+  const isLoadingLtpLoans = useSelector((state) => state.ltpLoans.isLoading);
+  const dataErrorLtpLoans = useSelector((state) => state.ltpLoans.error);
+  const dataStatusCodeLtpLoans = useSelector(
+    (state) => state.ltpLoans.statusCode
+  );
+  const dataErrorUrlLtpLoans = useSelector(
+    (state) => state.ltpLoans.dataErrorUrl
+  );
+  const isLoadingServiceMeasures = useSelector(
+    (state) => state.serviceMeasures.isLoading
+  );
+  const dataErrorServiceMeasures = useSelector(
+    (state) => state.serviceMeasures.error
+  );
+  const dataStatusCodeServiceMeasures = useSelector(
+    (state) => state.serviceMeasures.statusCode
+  );
+  const dataErrorUrlServiceMeasures = useSelector(
+    (state) => state.serviceMeasures.dataErrorUrl
+  );
+  const dataErrorCalibrationExpiry = useSelector(
+    (state) => state.calibrationExpiry.error
+  );
+  const dataStatusCodeCalibrationExpiry = useSelector(
+    (state) => state.calibrationExpiry.statusCode
+  );
+  const dataErrorUrlCalibrationExpiry = useSelector(
+    (state) => state.calibrationExpiry.dataErrorUrl
+  );
   const [isRefreshNeeded, setIsRefreshNeeded] = useState(false);
   const baseStyles = windowDim && getBaseStyles(windowDim);
 
@@ -51,6 +104,8 @@ export default NotificationsScreen = (props) => {
       'in calibrationExpiry getItems userApiFetchParamsObj',
       userApiFetchParamsObj
     );
+    dispatch(getLtpLoansRequest(userApiFetchParamsObj)), [];
+    dispatch(getServiceMeasuresRequest(userApiFetchParamsObj)), [];
     dispatch(getCalibrationExpiryRequest(userApiFetchParamsObj)), [];
   });
 
@@ -71,7 +126,7 @@ export default NotificationsScreen = (props) => {
 
   //   useEffect(() => {
   //     // runs only once
-  //     // console.log('in stats use effect');
+  //     // console.log('in notifications use effect');
   //     const getItemsAsync = async () => {
   //       setIsRefreshNeeded(false);
   //       getItems();
@@ -85,6 +140,109 @@ export default NotificationsScreen = (props) => {
   //     didFocusSubscription.remove();
   //     setIsRefreshNeeded(true);
   //   });
+
+  const getLtpLoanStatus = (item) => {
+    console.log(
+      'tool',
+      item.toolNr,
+      'now',
+      now,
+      'startDate',
+      item.startDate,
+      'expiryDate',
+      item.endDateDue
+    );
+    let theFromDate = null;
+    let theToDate = null;
+    let ageOfExpiry = 0;
+    let ageOfStart = 0;
+
+    if (item.collectedDate && item.collectionNumber) {
+      return false;
+    }
+
+    if (item.endDateDue && item.endDateDue.length > 0) {
+      theToDate = moment(item.endDateDue, 'DD/MM/YYYY HH:mm:ss');
+      ageOfExpiry = (now && now.diff(moment(theToDate), 'days')) || 0;
+    }
+    console.log('ageOfExpiry', ageOfExpiry);
+
+    if (ageOfExpiry >= -2) {
+      return false;
+    } else {
+      if (item.startDate && item.startDate.length > 0) {
+        theFromDate = moment(item.startDate, 'DD/MM/YYYY HH:mm:ss');
+        ageOfStart = (now && now.diff(moment(theFromDate), 'days')) || 0;
+        console.log('ageOfStart', ageOfStart, moment(theFromDate));
+      }
+
+      if (ageOfStart >= -3) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const getServiceMeasureStatus = (item) => {
+    // console.log(
+    //   'menuText',
+    //   item.menuText,
+    //   'now',
+    //   now,
+    //   'startDate',
+    //   item.startDate,
+    //   'expiryDate',
+    //   item.expiryDate
+    // );
+    let theFromDate = null;
+    let theToDate = null;
+    let ageOfExpiry = 0;
+    let ageOfStart = 0;
+
+    if (item.expiryDate && item.expiryDate.length > 0) {
+      theToDate = moment(item.expiryDate, 'DD/MM/YYYY HH:mm:ss');
+      ageOfExpiry = (now && now.diff(moment(theToDate), 'days')) || 0;
+    }
+    // console.log('ageOfExpiry', ageOfExpiry);
+
+    if (ageOfExpiry >= 0) {
+      return false;
+    } else {
+      if (item.startDate && item.startDate.length > 0) {
+        theFromDate = moment(item.startDate, 'DD/MM/YYYY HH:mm:ss');
+        ageOfStart = (now && now.diff(moment(theFromDate), 'days')) || 0;
+        // console.log('ageOfStart', ageOfStart, moment(theFromDate));
+      }
+
+      if (ageOfStart >= 0) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const filterServiceMeasuresItems = (serviceMeasuresItems) => {
+    let serviceMeasuresItemsFiltered = [];
+    if (serviceMeasuresItems && serviceMeasuresItems.length > 0) {
+      serviceMeasuresItemsFiltered = serviceMeasuresItems.filter(
+        (item) =>
+          item.startDate && item.expiryDate && getServiceMeasureStatus(item)
+      );
+    }
+    // console.log('serviceMeasuresItemsFiltered', serviceMeasuresItemsFiltered);
+    return serviceMeasuresItemsFiltered;
+  };
+
+  const filterLtpLoansItems = (ltpLoansItems) => {
+    let ltpLoansItemsFiltered = [];
+    if (ltpLoansItems && ltpLoansItems.length > 0) {
+      ltpLoansItemsFiltered = ltpLoansItems.filter(
+        (item) => item.startDate && item.endDateDue && getLtpLoanStatus(item)
+      );
+    }
+    console.log('LtpLoansItemsFiltered', ltpLoansItemsFiltered);
+    return ltpLoansItemsFiltered;
+  };
 
   const filterCalibrationExpiryItems = (calibrationExpiryItems) => {
     let calibrationExpiryItemsFiltered = [];
@@ -104,6 +262,57 @@ export default NotificationsScreen = (props) => {
     return calibrationExpiryItemsFiltered;
   };
 
+  useEffect(() => {
+    if (
+      isLoadingCalibrationExpiry ||
+      isLoadingLtpLoans ||
+      isLoadingServiceMeasures
+    ) {
+      setIsLoadingAny(true);
+    } else {
+      setIsLoadingAny(false);
+    }
+  }, [isLoadingCalibrationExpiry, isLoadingLtpLoans, isLoadingServiceMeasures]);
+
+  useEffect(() => {
+    // runs only once
+    if (dataErrorLtpLoans) {
+      setDataErrorAny(dataErrorLtpLoans);
+      setDataStatusCodeAny(dataStatusCodeLtpLoans);
+      setDataErrorUrlAny(dataErrorUrlLtpLoans);
+      setDataErrorSummary('Error syncing LtpLoans');
+      setDataNameInPlay('LTP loans');
+    } else if (dataErrorServiceMeasures) {
+      setDataErrorAny(dataErrorServiceMeasures);
+      setDataStatusCodeAny(dataStatusCodeServiceMeasures);
+      setDataErrorUrlAny(dataErrorUrlServiceMeasures);
+      setDataErrorSummary('Error syncing service measures');
+      setDataNameInPlay('service measures');
+    } else if (dataErrorCalibrationExpiry) {
+      setDataErrorAny(dataErrorCalibrationExpiry);
+      setDataStatusCodeAny(dataStatusCodeCalibrationExpiry);
+      setDataErrorUrlAny(dataErrorUrlCalibrationExpiry);
+      setDataErrorSummary('Error syncing calibration expiry');
+      setDataNameInPlay('calibration expiry');
+    } else {
+      setDataErrorAny('');
+      setDataStatusCodeAny('');
+      setDataErrorUrlAny('');
+      setDataErrorSummary('');
+      setDataNameInPlay('');
+    }
+  }, [
+    dataErrorLtpLoans,
+    dataErrorServiceMeasures,
+    dataErrorCalibrationExpiry,
+    ltpLoansItems,
+    serviceMeasuresItems,
+    calibrationExpiryItems,
+    isLoadingLtpLoans,
+    isLoadingServiceMeasures,
+    isLoadingCalibrationExpiry,
+  ]);
+
   useFocusEffect(
     useCallback(() => {
       //   dispatch(revalidateUserCredentials({ calledBy: 'NotificationsScreen' }));
@@ -119,59 +328,149 @@ export default NotificationsScreen = (props) => {
   //     (userDataObj && Object.keys(userDataObj).length > 0) || 0;
 
   //   if (userDataPresent === true) {
-  //     // console.log('in stats screen,userDataObj OK', userDataPresent);
+  //     // console.log('in notifications screen,userDataObj OK', userDataPresent);
   //   } else {
-  //     // console.log('in stats screen, no userDataObj');
+  //     // console.log('in notifications screen, no userDataObj');
   //     getItems();
   //   }
 
-  //   let itemsToShow = !isLoading ? filterCalibrationExpiryItems(calibrationExpiryItems) : [];
-  let itemsToShow =
-    !isLoading && !dataError
+  //   let calibrationExpiryItemsToShow = !isLoadingCalibrationExpiry ? filterCalibrationExpiryItems(calibrationExpiryItems) : [];
+  let calibrationExpiryItemsToShow =
+    !isLoadingCalibrationExpiry && !dataErrorCalibrationExpiry
       ? demoModeOn
         ? filterCalibrationExpiryItems(calibrationExpiryDummyData)
         : filterCalibrationExpiryItems(calibrationExpiryItems)
       : [];
 
-  console.log('rendering Notifications screen');
+  let ltpLoansItemsToShow =
+    !isLoadingLtpLoans && !dataErrorLtpLoans
+      ? demoModeOn
+        ? filterLtpLoansItems(ltpLoansDummyData)
+        : filterLtpLoansItems(ltpLoansItems)
+      : [];
+
+  let serviceMeasuresItemsToShow =
+    !isLoadingServiceMeasures && !dataErrorServiceMeasures
+      ? demoModeOn
+        ? filterServiceMeasuresItems(serviceMeasuresDummyData)
+        : filterServiceMeasuresItems(serviceMeasuresItems)
+      : [];
+
+  console.log(
+    'rendering Notifications screen',
+    calibrationExpiryItemsToShow,
+    'isLoadingCalibrationExpiry',
+    isLoadingCalibrationExpiry,
+    'dataErrorCalibrationExpiry',
+    dataErrorCalibrationExpiry
+  );
+  console.log(
+    'rendering Notifications screen',
+    serviceMeasuresItemsToShow.length,
+    'isLoadingServiceMeasures',
+    isLoadingServiceMeasures,
+    'dataErrorServiceMeasures',
+    dataErrorServiceMeasures
+  );
+
+  console.log(
+    'rendering Notifications screen',
+    ltpLoansItemsToShow.length,
+    'isLoadingLtpLoans',
+    isLoadingLtpLoans,
+    'dataErrorLtpLoansItems',
+    dataErrorLtpLoans
+  );
 
   return (
-    <View>
+    <ScrollView>
       {demoModeOn ? (
         <View style={baseStyles.viewPromptRibbonNoneFound}>
           <Text style={baseStyles.textPromptRibbon}>
             Showing sample data for Lyndon.
           </Text>
         </View>
-      ) : dataError ? null : itemsToShow.length === 0 ? (
-        isLoading ? null : (
-          <View style={baseStyles.viewPromptRibbon}>
-            <Text style={baseStyles.textPromptRibbon}>
-              No calibration expiry notifications to show.
-            </Text>
-          </View>
-        )
       ) : null}
-      {!isLoading ? (
-        dataError ? (
+      {!isLoadingCalibrationExpiry ? (
+        dataErrorCalibrationExpiry ? (
           <ErrorDetails
             errorSummary={'Error syncing calibration expiry'}
-            dataStatusCode={dataStatusCode}
-            errorHtml={dataError}
-            dataErrorUrl={dataErrorUrl}
+            dataStatusCode={dataStatusCodeCalibrationExpiry}
+            errorHtml={dataErrorCalibrationExpiry}
+            dataErrorUrl={dataErrorUrlCalibrationExpiry}
           />
-        ) : itemsToShow.length > 0 ? (
+        ) : (
           <View>
             <View style={baseStyles.viewPromptRibbon}>
               <Text style={baseStyles.textPromptRibbon}>
-                Calibration expiry coming up
+                Calibration expiry
               </Text>
             </View>
-            <CalibrationExpiryList items={itemsToShow} />
+            {calibrationExpiryItemsToShow.length > 0 ? (
+              <CalibrationExpiryList items={calibrationExpiryItemsToShow} />
+            ) : (
+              <View style={baseStyles.viewDataList}>
+                <View style={baseStyles.textDataListItem}>
+                  <Text>No calibration expirations to show.</Text>
+                </View>
+              </View>
+            )}
           </View>
-        ) : null
+        )
       ) : null}
-    </View>
+      {!isLoadingLtpLoans ? (
+        dataErrorLtpLoans ? (
+          <ErrorDetails
+            errorSummary={'Error syncing LTP loans'}
+            dataStatusCode={dataStatusCodeLtpLoans}
+            errorHtml={dataErrorLtpLoans}
+            dataErrorUrl={dataErrorUrlLtpLoans}
+          />
+        ) : (
+          <View>
+            <View style={baseStyles.viewPromptRibbon}>
+              <Text style={baseStyles.textPromptRibbon}>Current LTP loans</Text>
+            </View>
+            {ltpLoansItemsToShow.length > 0 ? (
+              <LtpLoansList items={ltpLoansItemsToShow} />
+            ) : (
+              <View style={baseStyles.viewDataList}>
+                <View style={baseStyles.textDataListItem}>
+                  <Text>No LTP loans to show.</Text>
+                </View>
+              </View>
+            )}
+          </View>
+        )
+      ) : null}
+      {!isLoadingServiceMeasures ? (
+        dataErrorServiceMeasures ? (
+          <ErrorDetails
+            errorSummary={'Error syncing service measures'}
+            dataStatusCode={dataStatusCodeServiceMeasures}
+            errorHtml={dataErrorServiceMeasures}
+            dataErrorUrl={dataErrorUrlServiceMeasures}
+          />
+        ) : (
+          <View>
+            <View style={baseStyles.viewPromptRibbon}>
+              <Text style={baseStyles.textPromptRibbon}>
+                Current service measures
+              </Text>
+            </View>
+            {serviceMeasuresItemsToShow.length > 0 ? (
+              <ServiceMeasuresList items={serviceMeasuresItemsToShow} />
+            ) : (
+              <View style={baseStyles.viewDataList}>
+                <View style={baseStyles.textDataListItem}>
+                  <Text>No service measures to show.</Text>
+                </View>
+              </View>
+            )}
+          </View>
+        )
+      ) : null}
+    </ScrollView>
   );
 };
 const titleString = 'Notifications';
