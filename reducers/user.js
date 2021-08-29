0931a4d790a65d0b0259a3c5e92df0b4e1defbb6
Alan Upstone <alan@helpfulconsultants.com>
2021-08-29 12:52:34 +0100

    set showingOldApp: false, on sign-in

--- reducers/user.js
+++ reducers/user.js
@@ -20,7 +20,7 @@ const INITIAL_STATE = {
   dataErrorUrl: null,
   statusCode: null,
   requestedDemo: false,
-  showingOldApp: true,
+  showingOldApp: false,
 };
 
 export default function user(state = INITIAL_STATE, action) {
@@ -138,6 +138,7 @@ export default function user(state = INITIAL_STATE, action) {
       //   console.log('%%% user reducer -userBrand in reducer is ', userBrand);
       return {
         ...state,
+        showingOldApp: false,
         userIsSignedIn: true,
         userIsValidated: true,
         userData: action.payload.items,
