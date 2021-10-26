f7fd1cbaeacc0ccdee762d2b2b96e28978698344
Alan Upstone <alan@helpfulconsultants.com>
2021-10-26 13:30:35 +0100

    app numbers

--- dummyData/calibrationExpiryDummyData.js
+++ dummyData/calibrationExpiryDummyData.js
@@ -1,7 +1,7 @@
 export default bookedOutToolsDummyData = [
-  { howMany: '0', expiry: '1. WARNING! EXPIRED' },
-  { howMany: '4', expiry: '2. Less than 30 days' },
-  { howMany: '1', expiry: '3. More than 30 days' },
+  { howMany: '1', expiry: '1. WARNING! EXPIRED' },
+  { howMany: '2', expiry: '2. Less than 30 days' },
+  { howMany: '7', expiry: '3. More than 30 days' },
   { howMany: '1', expiry: '4. More than 60 days' },
   { howMany: '4', expiry: '5. More than 90 days' },
 ];
