302a41b04f14d2bbb2781c47aacc7b7d4f9ff124
Alan Upstone <alan@helpfulconsultants.com>
2022-02-08 18:47:00 +0000

    update new dummydata

---
+++ dummyData/newsDummyData copy.js
@@ -0,0 +1,120 @@
+export default newsDummyData = [
+  {
+    id: '20',
+    newsType: 'News',
+    headline: 'How Important is a Service Measure?',
+    newstext:
+      'All Service Measures are a factory directive and must be adhered to in every case.. \nRead this document for full details on this compulsory requirement.',
+    imageName: 'Scales.png',
+    linkTo:
+      'https://toolsinfoweb.co.uk/content/documents/Workshop_Support/How_important_is_a_Service_Measure.pdf',
+    createdDate: '28/09/2021 20:51:33',
+    lastUpdated: '16/10/2021 11:44:03',
+    updatedBy: 'Lyndon Evans',
+    ipAddress: '165.225.17.25',
+    businessCritical: 'Y',
+  },
+  {
+    id: '56',
+    newsType: 'News',
+    headline: 'VAS 6558/9-6 and V6558/9-6A',
+    newstext:
+      'We have discovered that the high-voltage test adapters VAS 6558/9-6 (ASE 405 379 00 000) have been delivered with high-voltage cables that may only be used at voltages of up to 600 V AC/DC according to the text on the cables.',
+    imageName: 'highVoltgeCables.png',
+    linkTo:
+      'https://toolsinfoweb.co.uk/content/documents/Workshop_Support/High-voltage_test_adapters_VAS_6558_9-6_and_VAS_6558_9-6A.pdf',
+    createdDate: '03/11/2021 11:14:59',
+    lastUpdated: '',
+    updatedBy: 'Simon Groves',
+    ipAddress: '109.150.236.17',
+    businessCritical: 'Y',
+  },
+  {
+    id: '55',
+    newsType: 'News',
+    headline: 'Support Ends for Win10 Enterprise LTSC',
+    newstext:
+      'The current Windows 10 LTSC 2016 version will reach the end of its mainstream support on 12th October 2021 after five years. Why not take this information as an opportunity to create restoration points on your units now?',
+    imageName: 'ltsc2016_end_Of_Life.png',
+    linkTo:
+      'https://toolsinfoweb.co.uk/content/documents/Mainstream_support_for_Win10_Enterprise_LTSC_2016_to_end.pdf',
+    createdDate: '15/10/2021 11:07:03',
+    lastUpdated: '15/10/2021 11:08:37',
+    updatedBy: 'Simon Groves',
+    ipAddress: '109.150.236.17',
+    businessCritical: 'N',
+  },
+  {
+    id: '7',
+    newsType: 'News',
+    headline: '*NEW* Wheel Alignment Software',
+    newstext:
+      'Click here to see a summary regarding affected models in the new Wheel alignment software for each approved manufacturer.',
+    imageName: 'Domino.jpg',
+    linkTo:
+      'https://toolsinfoweb.co.uk/content/documents/New Wheel Alignment Software Notification.pdf',
+    createdDate: '31/08/2018 11:26:05',
+    lastUpdated: '05/10/2021 12:42:03',
+    updatedBy: 'Beth Clarke',
+    ipAddress: '147.161.169.75',
+    businessCritical: 'N',
+  },
+  {
+    id: '54',
+    newsType: 'News',
+    headline: 'Pro Align VAS 6292',
+    newstext:
+      'Hunter Quick Check Drive matched with Hunter Quick Tread Edge contribute towards doubling your alignment and tyre revenue.',
+    imageName: 'proAlignHeadline.png',
+    linkTo: '?controller=Stories&action=View&id=71',
+    createdDate: '01/10/2021 11:11:24',
+    lastUpdated: '',
+    updatedBy: 'Simon Groves',
+    ipAddress: '82.132.237.122',
+    businessCritical: 'N',
+  },
+  {
+    id: '49',
+    newsType: 'News',
+    headline: 'NEW Volkswagen AG Tools & Equipment Catalogue',
+    newstext:
+      'The Factory has made available to us an updated version of their Sales Catalogue for Tools and Equipment. 892 pages of information about the latest products available.',
+    imageName: 'VW_AG_Sales_Catalogue_2021_thumbnail.png',
+    linkTo: 'VW_AG_Sales_Catalogue/Catalogue_2021_May',
+    createdDate: '21/05/2021 11:54:59',
+    lastUpdated: '14/09/2021 19:15:09',
+    updatedBy: 'Simon Groves',
+    ipAddress: '109.150.236.121',
+    businessCritical: 'N',
+  },
+  {
+    id: '53',
+    newsType: 'News',
+    headline: 'Remote Control Your WAECO Device !',
+    newstext:
+      'WORKS WITH ANY INTERNET-CAPABLE DEVICE. \nConveniently control your ASC service unit via a Wi-Fi link. All you need is the new WAECO ASC G Wi-Fi kit.',
+    imageName: 'waecoAirConService.png',
+    linkTo:
+      'https://toolsinfoweb.co.uk/content/documents/Sales_Support/WAECO_WIFI_Flyer.pdf',
+    createdDate: '30/07/2021 10:15:30',
+    lastUpdated: '14/09/2021 19:14:39',
+    updatedBy: 'Simon Groves',
+    ipAddress: '109.150.236.121',
+    businessCritical: 'N',
+  },
+  {
+    id: '8',
+    newsType: 'News',
+    headline: 'Digital Refractometer VAS 531005',
+    newstext:
+      'For transparent performance and efficient operation! Thanks to its universal scale of measurement, the\nRefractometer VAS 531 005 makes it possible for the first time to analyze a variety of different fluids, making it quicker and easier to check the anti-freeze protection level of engine coolant and windscreen cleaning fluid, as well as the urea concentration of AdBlue\u0197\u0180.',
+    imageName: 'boyWithTools.png',
+    linkTo:
+      'https://toolsinfoweb.co.uk/content/documents/inventory/21/Digital-Refractometer_VAS_531005.pdf',
+    createdDate: '31/08/2018 11:27:38',
+    lastUpdated: '14/09/2021 19:13:00',
+    updatedBy: 'Simon Groves',
+    ipAddress: '109.150.236.121',
+    businessCritical: 'N',
+  },
+];
