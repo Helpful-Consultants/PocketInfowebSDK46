ce9cd70e0273114cfe32e17f7816fd1e00c4db53
Alan Upstone <alan@helpfulconsultants.com>
2023-12-01 20:06:51 +0000

    change info string

--- helpers/appChangeInfoString.js
+++ helpers/appChangeInfoString.js
@@ -1,4 +1,4 @@
-const appChangeInfoString = 'Changes: Elsa2Go available.';
+const appChangeInfoString = 'Changes: Elsa2Go is available.';
 
 // const getAppInfo = () => {
 //   //   console.log(objArr.length, key, order);
