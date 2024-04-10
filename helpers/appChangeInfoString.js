9f79437bf25fc2eea730988b02a9e337d7fa7688
Alan Upstone <alan@helpfulconsultants.com>
2024-04-10 17:49:01 +0100

    build extra to test

--- helpers/appChangeInfoString.js
+++ helpers/appChangeInfoString.js
@@ -1,4 +1,4 @@
-const appChangeInfoString = 'Changes: Formatting';
+const appChangeInfoString = 'Changes: SDK 50';
 
 // const getAppInfo = () => {
 //   //   console.log(objArr.length, key, order);
