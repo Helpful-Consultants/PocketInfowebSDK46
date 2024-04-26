37d4482e453df6412e38b45c4f6113480009e015
Alan Upstone <alan@helpfulconsultants.com>
2024-04-26 19:46:53 +0100

    remove redundant import

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,6 @@ import { useFocusEffect } from '@react-navigation/native';
 import * as Updates from 'expo-updates';
 import * as Notifications from 'expo-notifications';
 import Constants from 'expo-constants';
-import * as Application from 'expo-application';
 // import * as Permissions from 'expo-permissions';
 import {
   ActivityIndicator,
