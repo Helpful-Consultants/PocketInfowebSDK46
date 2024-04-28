06b60a71b441d313193daf1baf3ede8aba3292c1
Alan Upstone <alan@helpfulconsultants.com>
2024-04-28 15:26:51 +0100

    add flex to keyboard view to avoid hang https://github.com/facebook/react-native/issues/42939

--- helpers/getBaseStyles.js
+++ helpers/getBaseStyles.js
@@ -320,6 +320,7 @@ export default getBaseStyles = (props) => {
     },
     containerSignIn: {
       marginHorizontal: paddingSize,
+      flex: 1,
     },
     // LtpListScreen
     containerFlexAndMargin: {
