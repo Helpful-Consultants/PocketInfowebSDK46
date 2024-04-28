400461c1e3dfbbb8947c087f03ceb50485f3e6c4
Alan Upstone <alan@helpfulconsultants.com>
2024-04-28 14:49:54 +0100

    Revert "restore sign-in page"
    This reverts commit 2a959e0302bbb9820ed09e27e47420884d20f9c4.

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -346,7 +346,7 @@ export default AppNavigator = (props) => {
 
   return (
     <NavigationContainer>
-      {allOK === true ? <DrawerNavigator /> : <SignedOutStack />}
+      <DrawerNavigator />
     </NavigationContainer>
   );
 };
