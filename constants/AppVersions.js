81eadc1198fee5563f16d4c51527f2734d15bc04
Alan Upstone <alan@helpfulconsultants.com>
2024-04-28 17:44:23 +0100

    change info string to test eas updates

--- constants/AppVersions.js
+++ constants/AppVersions.js
@@ -5,4 +5,4 @@ export const AppStoreBuildNumbers = {
   IOS_EXTRA: '141', // it is a string
 };
 
-export const APP_CHANGE_INFO_STRING = 'Changes: SDK 50 N KB';
+export const APP_CHANGE_INFO_STRING = 'Changes: Notifications';
