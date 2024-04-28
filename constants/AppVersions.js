0b0868d06e80922f791a6988676fa6b181de403d
Alan Upstone <alan@helpfulconsultants.com>
2024-04-28 21:48:56 +0100

    android versions

--- constants/AppVersions.js
+++ constants/AppVersions.js
@@ -1,6 +1,6 @@
 export const AppStoreBuildNumbers = {
-  ANDROID_PRO: 33, // it is a number
-  ANDROID_EXTRA: 34, // it is a number
+  ANDROID_PRO: 36, // it is a number
+  ANDROID_EXTRA: 36, // it is a number
   IOS_PRO: '142', // it is a string
   IOS_EXTRA: '141', // it is a string
 };
