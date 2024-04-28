856bde8855a1cc70f7c071c4fa8a0301de9b9817
Alan Upstone <alan@helpfulconsultants.com>
2024-04-28 20:36:48 +0100

    more alerts

--- helpers/getNavTargetObj.js
+++ helpers/getNavTargetObj.js
@@ -1,32 +1,32 @@
 import { AppSectionsByScreen, ScreenNames } from '../constants/AppParts';
 
 const getNavTargetObj = (title) => {
-  console.log('in getNavTargetObj, start ', title && title);
+  alert('in getNavTargetObj, start ' + title && title);
   // Input validation
   if (!title) {
-    console.error('Invalid title:', title);
+    alert('Invalid title:' + title);
     return null; // or throw an error
   }
   // Normalize title and convert to uppercase
   const constantFromPropsTitle = title?.replace(/\s/g, '').toUpperCase();
-  alert('constantFromPropsTitle', constantFromPropsTitle);
+  alert('constantFromPropsTitle' + constantFromPropsTitle);
   // Get the corresponding app section from constants
   const appSectionToUse = AppSectionsByScreen?.[constantFromPropsTitle] ?? null;
-  alert('appSectionToUse', appSectionToUse);
+  alert('appSectionToUse' + appSectionToUse);
   // Get the screen name from constants, defaulting to 'DEFAULT' if not found
   const titleToUse =
     ScreenNames?.[constantFromPropsTitle] ?? ScreenNames.DEFAULT;
 
-  console.log(
-    'in getNavTargetObj, constantFromPropsTitle',
-    constantFromPropsTitle
+  alert(
+    'in getNavTargetObj, constantFromPropsTitle' +
+      JSON.stringify(constantFromPropsTitle)
   );
 
   const returnObj = {
     targetSection: appSectionToUse,
     targetScreen: titleToUse,
   };
-  console.log('in getNavTargetObj, returnObj', returnObj);
+  alert('in getNavTargetObj, returnObj' + JSON.stringify(returnObj));
   return returnObj;
 };
 export default getNavTargetObj;
