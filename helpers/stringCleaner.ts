1e8f2708091ac774838444830c998f3df7142201
Alan Upstone <alan@helpfulconsultants.com>
2024-05-01 12:30:39 +0100

    linted sctringcleaner

--- helpers/stringCleaner.ts
+++ helpers/stringCleaner.ts
@@ -1,39 +1,33 @@
-const stringCleaner = (rawString: string = '') => {
-  if (rawString && rawString.length > 0) {
-    const rawStringLowerCase = rawString.toLowerCase();
+const stringCleaner = (rawString = '') => {
+  if (rawString.length === 0) {
+    return rawString;
+  }
 
-    if (
-      rawStringLowerCase.substring(0, 4) === 'ase ' ||
-      rawStringLowerCase.substring(0, 4) === 'vas ' ||
-      rawStringLowerCase.substring(0, 4) === 'vag '
-    ) {
-      if (
-        rawStringLowerCase.substring(0, 5) ===
-        ('0' || '1' || '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9')
-      ) {
-        return rawString.substr(4);
-      } else {
-        return rawString;
-      }
-    } else if (
-      rawStringLowerCase.substring(0, 3) === 'ase' ||
-      rawStringLowerCase.substring(0, 3) === 'vas' ||
-      rawStringLowerCase.substring(0, 3) === 'vag'
-    ) {
-      if (
-        rawStringLowerCase.substring(0, 4) ===
-        ('0' || '1' || '2' || '3' || '4' || '5' || '6' || '7' || '8' || '9')
-      ) {
-        return rawString.substr(3);
-      } else {
-        return rawString;
-      }
+  const rawStringLowerCase = rawString.toLowerCase();
+
+  if (
+    rawStringLowerCase.startsWith('ase ') ||
+    rawStringLowerCase.startsWith('vas ') ||
+    rawStringLowerCase.startsWith('vag ')
+  ) {
+    if (/^\d/.test(rawStringLowerCase)) {
+      return rawString.substr(4);
     } else {
       return rawString;
     }
+  } else if (
+    rawStringLowerCase.startsWith('ase') ||
+    rawStringLowerCase.startsWith('vas') ||
+    rawStringLowerCase.startsWith('vag')
+  ) {
+    if (/^\d/.test(rawStringLowerCase)) {
+      return rawString.substr(3);
+    } else {
+      return rawString;
+    }
+  } else {
+    return rawString;
   }
-
-  return rawString;
 };
 
-export default stringCleaner
+export default stringCleaner;
