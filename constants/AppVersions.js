19f481b4227eae668255394cab184a700e2267ea
Alan Upstone <alan@helpfulconsultants.com>
2024-09-02 12:45:51 +0100

    update v for store

--- constants/AppVersions.js
+++ constants/AppVersions.js
@@ -1,8 +1,8 @@
 export const AppStoreBuildNumbers = {
-  ANDROID_PRO: 36, // it is a number
-  ANDROID_EXTRA: 36, // it is a number
+  ANDROID_PRO: 37, // it is a number
+  ANDROID_EXTRA: 37, // it is a number
   IOS_PRO: '142', // it is a string
-  IOS_EXTRA: '141', // it is a string
+  IOS_EXTRA: '142', // it is a string
 };
 
 export const APP_CHANGE_INFO_STRING = 'Changes: Corporate Identity.';
