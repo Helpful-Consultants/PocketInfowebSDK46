b7417638ae3f5d77aa1016417a22145da661c982
Alan Upstone <alan@helpfulconsultants.com>
2024-09-04 09:23:15 +0100

    eas update

--- constants/AppVersions.js
+++ constants/AppVersions.js
@@ -5,4 +5,4 @@ export const AppStoreBuildNumbers = {
   IOS_EXTRA: '143', // it is a string
 };
 
-export const APP_CHANGE_INFO_STRING = 'Changes: Corporate Identity.';
+export const APP_CHANGE_INFO_STRING = 'Changes: Corporate Identity';
